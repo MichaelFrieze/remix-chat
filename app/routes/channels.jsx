0a9cb850d1ee637fb6242ddd30f384b6f735fae8
Michael Frieze <michaelfrieze@icloud.com>
2022-04-10 23:21:12 -0400

    styling the channels route

--- app/routes/channels.jsx
+++ app/routes/channels.jsx
@@ -19,13 +19,20 @@ export default () => {
   const { channels } = useLoaderData();
 
   return (
-    <div>
-      {channels.map((channel) => (
-        <p key={channel.id} className="text-3xl">
-          <Link to={`/channels/${channel.id}`}>{channel.title}</Link>
-        </p>
-      ))}
-      <Outlet />
+    <div className="h-screen flex">
+      <div className="bg-gray-800 text-white w-40 p-8">
+        {channels.map((channel) => (
+          <p key={channel.id}>
+            <Link to={`/channels/${channel.id}`}>
+              <span className="text-gray-400 mr-1">#</span>
+              {channel.title}
+            </Link>
+          </p>
+        ))}
+      </div>
+      <div className="flex-1 p-8 flex flex-col">
+        <Outlet />
+      </div>
     </div>
   );
 };
